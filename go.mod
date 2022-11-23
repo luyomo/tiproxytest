24b309f7671495ed75ead5570df6b58cf734aec2
Debian <admin@ip-172-82-11-164.ec2.internal>
2022-11-23 06:26:01 +0000

    Added server restart service

--- go.mod
+++ go.mod
@@ -6,9 +6,12 @@ require (
 	github.com/AstroProfundis/tabby v1.1.1 // indirect
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/ScaleFT/sshkeys v1.2.0 // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/dchest/bcrypt_pbkdf v0.0.0-20150205184540-83f37f9c154a // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/joomcode/errorx v1.1.0 // indirect
@@ -25,6 +28,9 @@ require (
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	go.etcd.io/etcd/api/v3 v3.5.6 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.6 // indirect
+	go.etcd.io/etcd/client/v3 v3.5.6 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
@@ -33,6 +39,9 @@ require (
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/term v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20210930144712-2e2e1008e8a3 // indirect
+	google.golang.org/grpc v1.41.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,36 +2,115 @@ package main
 
 import (
     "database/sql"
+    "encoding/json"
     "fmt"
+    "context"
     "os"
     "time"
     "sync"
+    "regexp"
+    "os/exec"
+
     _ "github.com/go-sql-driver/mysql"
     "github.com/luyomo/tisample/pkg/tui"
+    clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 
 var LK = sync.RWMutex{}
 
+type Component struct {
+    IP string `json:"ip"`
+}
+
+
 func main(){
     fmt.Printf("Hello world. \n")
     pid := os.Getpid()
+
+    // RestartTiDBCluster()
+
+    ctx, cancel := context.WithCancel(context.Background())
 
     retry := 0
     var wg sync.WaitGroup
     wg.Add(2)
     go DBInsert(pid+1, &wg, &retry)
     go DBInsert(pid+2, &wg, &retry)
+    go RestartTiDBCluster(ctx)
 
 
     fmt.Println("Waiting for goroutines to finish... ")
     wg.Wait()
+    fmt.Printf("Starting to cancel all the processes \n\n\n")
+    cancel()
     fmt.Printf("Retried : <%d>\n", retry)
     fmt.Println("Done! ")
 
     tableOutput := [][]string{{"Expected Insert Row", "Actual Insert Row", "Execution Time", "# of Retry", "# of threads", "# of TiDB Restart"}}
     tableOutput = append(tableOutput, []string{"200000", "200000", "20 second", "3", "2", "3"} )
     tui.PrintTable(tableOutput, true)
+}
+
+func RestartTiDBCluster(ctx context.Context) {
+    var tidbIPs []string
+    client, err := clientv3.New(clientv3.Config{Endpoints:   []string{"182.83.1.86:2379"}  })
+    if err != nil {
+        panic(err)
+    }
+
+//    ctx, cancel := context.WithTimeout(client.Ctx(), 10 * time.Second)
+//    defer cancel()
+    members , err := client.MemberList(ctx)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("The members are <%#v> \n", members)
+
+    kv := clientv3.NewKV(client)
+    gr, _ := kv.Get(ctx, "/topology/tidb", clientv3.WithPrefix())
+    for _, tidbNode := range gr.Kvs {
+        match, _ := regexp.MatchString("/info$",string( tidbNode.Key) ) 
+	if match == true {
+            jsonData, err := componentFromJSON(string(tidbNode.Value) )
+            if err != nil {
+                panic(err)
+            }
+            fmt.Println("Value: ", string(tidbNode.Value), "Revision: ", string(tidbNode.Key))
+            fmt.Printf("The IP address is <%s> \n", jsonData.IP)
+            tidbIPs = append(tidbIPs, jsonData.IP)
+        }else{
+            fmt.Println("Unmatched -> Value: ", string(tidbNode.Value), "Revision: ", string(tidbNode.Key))
+        }
+    }
+
+    fmt.Printf("The IP is <%#v> \n", tidbIPs)
+    ticker := time.NewTicker(time.Duration(15) * time.Second)
+
+    for {
+         select {
+             case <-ticker.C:
+                 fmt.Printf("Starting to restart TiDB Node \n\n\n")
+                 tidbIPs = append(tidbIPs[1:], tidbIPs[0])
+
+                 cmd := exec.Command("/home/admin/.tiup/bin/tiup", "cluster", "restart", "mgtest", "-y", "--node", fmt.Sprintf("%s:4000", tidbIPs[0]))
+                 err := cmd.Run()
+                 if err != nil {
+                     fmt.Printf("The error is <%s> \n", err.Error())
+                 }
+             case <-ctx.Done():
+                 fmt.Printf("Starting to stop all the instances \n\n\n")
+                 return
+         }
+ 
+     }
+}
+
+func componentFromJSON(str string) (s Component, err error) {
+    if err = json.Unmarshal([]byte(str), &s); err != nil {
+        panic(err)
+    }
+    return
 }
 
 func PrepareDBConn() (error, *sql.DB, *sql.Stmt, *sql.Stmt) {
@@ -54,7 +133,6 @@ func PrepareDBConn() (error, *sql.DB, *sql.Stmt, *sql.Stmt) {
     if err != nil {
         panic(err.Error()) // proper error handling instead of panic in your app
     }
- //   defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
 
     // Prepare statement for reading data
     stmtOut, err := db.Prepare("SELECT squareNumber FROM squarenum WHERE number = ?")
